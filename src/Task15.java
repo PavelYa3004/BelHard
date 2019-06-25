d1704bfb99b3a04d667a2553f2cdc91899e83f19
PavelYa3004 <psp3004-3@inbox.ru>
2019-06-25 18:49:38 +0300

    Переделал

--- src/Task15.java
+++ src/Task15.java
@@ -8,15 +8,20 @@ public class Task15 {
     public static void main(String[] args) {
         Random r = new Random();
         int ar[] = new int[r.nextInt(15) + 15];
-        System.out.println(ar.length - 1);
-        for (int i = 1; i < ar.length - 1; i++) {
+        System.out.println(ar.length );
+        for (int i = 0; i < ar.length ; i++) {
         ar[i]=(int)(Math.random()*30);
             System.out.print(ar[i]+" ");
         }
+
         System.out.println(" ");
-        for (int i = 1; i < ar.length-1; i++) {
-            int x=ar[ar.length-1-i];
-            System.out.print(x+" ");
+        for (int i = 0; i < ar.length/2; i++) {
+           int x=ar[i];
+           ar[i]=ar[ar.length-1-i];
+           ar[ar.length-i-1]=x;
+        }
+        for (int i = 0; i <ar.length ; i++) {
+            System.out.print(ar[i]+" ");
 
         }
     }
